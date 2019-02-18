1bf83d1cca8ca5f1986c35db8996e4878448c21f
Dianne Jarvis-Lumsden <diannejarvisj@gmail.com>
2019-02-18 21:21:16 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,8 +1,8 @@
 class Application
 
   def call(env)
-    resp = Rack::Response.new
-    req = Rack::Request.new(env)
+    # resp = Rack::Response.new
+    # req = Rack::Request.new(env)
   #
   #   if req.path=="/songs"
   #     resp.write "You requested the songs"

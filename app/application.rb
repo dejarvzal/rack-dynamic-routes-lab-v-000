e9b7942dc4b58b3c341d04d550ae8147bbd99713
Dianne Jarvis-Lumsden <diannejarvisj@gmail.com>
2019-02-18 22:20:24 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -7,7 +7,7 @@ class Application
     # @@items = [Item.new]
     if req.path=="/items"
       @@items.collect do |item|
-        resp.write "#{Item.price}\n"
+        resp.write "#{item.price}\n"
         end
     else
       resp.write "Route not found"

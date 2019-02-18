b4b1e6c073a3e4cd277cb2e82912690eb2692008
Dianne Jarvis-Lumsden <diannejarvisj@gmail.com>
2019-02-18 22:16:50 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -4,23 +4,15 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-    if req.path.match(/items/)
-    
-         item_price = req.path.split("/items/").last #turn /songs/Sorry into Sorry
-         item = @@items.find{|s| s.price == item_price}
-    
-         resp.write item.price
-       end
-
     # @@items = [Item.new]
-    # if req.path=="/items"
-    #   @@items.each do |item|
-    #     resp.write "#{Item.price}\n"
-    #     end
-    # else
-    #   resp.write "Route not found"
-    #   resp.status = 404
-    # end
+    if req.path=="/items"
+      @@items.each do |item|
+        resp.write "#{Item.price}\n"
+        end
+    else
+      resp.write "Route not found"
+      resp.status = 404
+    end
 
     resp.finish
   end

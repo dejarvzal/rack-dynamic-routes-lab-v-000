a9ca5c674d616891c4d369c3a17a196f400eb283
Dianne Jarvis-Lumsden <diannejarvisj@gmail.com>
2019-02-18 22:28:17 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -10,7 +10,7 @@ class Application
         resp.write "#{item.price}\n"
         end
     else resp.write "Item not found"
-      elsif 
+      elsif
         resp.write "Route not found"
       resp.status = 404
     end

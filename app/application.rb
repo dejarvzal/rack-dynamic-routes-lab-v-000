104b6d043bde94ab545cf28cf88ee0c3cb35f15b
Dianne Jarvis-Lumsden <diannejarvisj@gmail.com>
2019-02-18 22:29:11 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -8,8 +8,9 @@ class Application
     if req.path=="/items"
       @@items.collect do |item|
         resp.write "#{item.price}\n"
-        end
+        # end
     else resp.write "Item not found"
+      
       elsif
         resp.write "Route not found"
       resp.status = 404

1710a2ed0192e9eeeb4ac6296dc20e80aa047d54
Dianne Jarvis-Lumsden <diannejarvisj@gmail.com>
2019-02-18 22:50:33 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,16 +3,11 @@ class Application
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
-    #
-    # if req.path.match(/items/)
-    #   @@items.each do |item|
-    #     resp.write "#{item.price}\n"
-    #     end
-      # elsif !@@items
+  
 
       if req.path.match(/items/)
-            item_name = req.path.split("/items/").last
-            if item =@@items.find{|i| i.name == item_name}
+        item_name = req.path.split("/items/").last
+          if item =@@items.find{|i| i.name == item_name}
               resp.write item.price
             else
               resp.status = 400
@@ -20,9 +15,8 @@ class Application
             end
           else
             resp.write "Route not found"
-      resp.status = 404
-    end
-
+            resp.status = 404
+      end
     resp.finish
   end
 end
